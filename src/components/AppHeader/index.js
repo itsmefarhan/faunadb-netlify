39e35ae456f27d0a99aa154c06fbc015c8ccef08
Farhan Farooq <farhan.farooq@live.com>
2020-10-23 13:27:23 +0500

    readme

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import deployButton from '../../assets/deploy-to-netlify.svg'
 import logo from '../../assets/logo.svg'
 import github from '../../assets/github.svg'
-import styles from './AppHeader.css' // eslint-disable-line
 
-const AppHeader = (props) => {
+const AppHeader = () => {
   return (
     <header className='app-header'>
       <div className='app-title-wrapper'>
